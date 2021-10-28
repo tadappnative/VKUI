d2309af22a1d930b307acdc62273aa73159ee89c
Evgeniya <e.polozova@corp.vk.com>
2021-10-28 15:29:08 +0300

    lib/warnOnce: move env check inside helper

--- src/lib/warnOnce.ts
+++ src/lib/warnOnce.ts
@@ -1,7 +1,8 @@
 export function warnOnce(zone: string) {
   const didWarn: { [msg: string]: boolean } = {};
-  return (message: string) => {
-    if (!didWarn[message]) {
+
+  return (message: string, env = 'development') => {
+    if (!didWarn[message] && process.env.NODE_ENV === env) {
       console.error(`[VKUI/${zone}] ${message}`);
       didWarn[message] = true;
     }
