ffbf72037d8ee0efafaecd361d4d7feb616b128e
Evgeniya <e.polozova@corp.vk.com>
2021-10-28 15:42:32 +0300

    IconButton: add deprecation warning

--- src/lib/warnOnce.ts
+++ src/lib/warnOnce.ts
@@ -1,8 +1,14 @@
-export function warnOnce(zone: string) {
+import { noop } from './utils';
+
+export function warnOnce(zone: string, env = 'development') {
+  if (process.env.NODE_ENV !== env) {
+    return noop;
+  }
+
   const didWarn: { [msg: string]: boolean } = {};
 
-  return (message: string, env = 'development') => {
-    if (!didWarn[message] && process.env.NODE_ENV === env) {
+  return (message: string) => {
+    if (!didWarn[message]) {
       console.error(`[VKUI/${zone}] ${message}`);
       didWarn[message] = true;
     }
